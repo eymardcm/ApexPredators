ee34dce2d9328ac1f4b9bbdf6c562dccb6b8b979
Chad Eymard <eymardcm@gmail.com>
2024-04-06 12:14:19 -0400

    Initial Commit

---
+++ ApexPredators/ApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApexPredatorsApp.swift
+//  ApexPredators
+//
+//  Created by Chad Eymard on 4/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
